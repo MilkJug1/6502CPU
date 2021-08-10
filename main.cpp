dd375825eeba160b8470b01cfd4ce051fa09e9a8
MilkJug <76970674+waterbottle-123@users.noreply.github.com>
2021-08-09 22:06:06 -0400

    added more cpu functions

--- main.cpp
+++ main.cpp
@@ -9,14 +9,28 @@
 ===========================================
 */
 
+using Byte = unsigned char;
+using Word = unsigned short;
+
+using u32 = unsigned int;
+struct Mem
+{
+    static constexpr u32 MAX_MEM = 1024 * 64;
+    Byte Data[MAX_MEM];
+
+    void Initialise() 
+    {
+        for ( u32 i = 0; i < MAX_MEM; i++)
+        {
+            Data[i] = 0;
+        }
+    }
+
+};
 
 
 struct CPU
 {
-    using Byte = unsigned char;
-    using Word = unsigned short;
-
-
     Word PC;    //program pointer
     Word SP;    // stack pointer
 
@@ -31,10 +45,28 @@ struct CPU
     Byte V : 1; // status flag
     Byte N : 1; // status flag
 
+    void Reset( Mem& memory )
+    {
+        PC = 0xFFFC;
+        SP = 0x0100;
+        C = Z = I = D = B = V = N = 0;
+        A = X = Y = 0;
+        memory.Initialise();
+    }
+
+    void Execute( u32 Ticks, Mem& memory )
+    {
+
+    }
+
 };
 
 
 int main()
 {
+    Mem mem;
     CPU cpu;
+    cpu.Reset( mem );
+    cpu.Execute();
+    return 0; 
 }
