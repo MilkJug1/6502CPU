725a76f86505560d0168e00364e5530aafbbd261
MilkJug <76970674+waterbottle-123@users.noreply.github.com>
2021-08-12 19:13:35 -0400

    added ReadyByte and ZeroPageAddress

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 = Credits goes to Dave Poo on Youtube
 = -for making that tutorial
 = Date Created 08/09/2021 (August 9, 2021)
+= Last Edited on 08/12/2021 at 07:11 PM
+= Last Edit from Michael Crawford
 ===========================================
 */
 
@@ -18,39 +20,36 @@ struct Mem
     static constexpr u32 MAX_MEM = 1024 * 64;
     Byte Data[MAX_MEM];
 
-    void Initialise() 
+    void Initialise()
     {
-        for ( u32 i = 0; i < MAX_MEM; i++)
+        for (u32 i = 0; i < MAX_MEM; i++)
         {
             Data[i] = 0;
         }
     }
 
     // read 1 byte
-    Byte operator[] ( u32 Address ) const
+    Byte operator[](u32 Address) const
     {
         // assert here  is < MAX_MEM
         return Data[Address];
     }
-
 
     // write 1 byte
-    Byte& operator[] ( u32 Address ) 
+    Byte &operator[](u32 Address)
     {
         // assert here  is < MAX_MEM
         return Data[Address];
     }
-
 };
 
-//* the main thing 
+//* the main thing
 struct CPU
 {
-    Word PC;    //program pointer
-    Byte SP;    // stack pointer
+    Word PC; //program pointer
+    Byte SP; // stack pointer
 
-
-    Byte A, X, Y;   //registers 
+    Byte A, X, Y; //registers
 
     Byte C : 1; // status flag
     Byte Z : 1; // status flag
@@ -60,7 +59,7 @@ struct CPU
     Byte V : 1; // status flag
     Byte N : 1; // status flag
 
-    void Reset( Mem& memory )
+    void Reset(Mem &memory)
     {
         PC = 0xFFFC;
         SP = 0x0100;
@@ -69,10 +68,17 @@ struct CPU
         memory.Initialise();
     }
 
-    Byte FetchByte( u32 Cycles, Mem& memory )
+    Byte FetchByte(u32 Cycles, Mem &memory)
     {
         Byte Data = memory[PC];
-        PC++ ;
+        PC++;
+        Cycles--;
+        return Data;
+    }
+
+    Byte Read(u32 Cycles, Mem &memory)
+    {
+        Byte Data = memory[PC];
         Cycles--;
         return Data;
     }
@@ -81,43 +87,48 @@ struct CPU
     static constexpr Byte
         INS_LDA_IM = 0xA9;
 
-    // Executes instructions 
-    void Execute( u32 Cycles, Mem& memory )
+    // Executes instructions
+    void Execute(u32 Cycles, Mem &memory)
     {
-        while( Cycles > 0)
+        while (Cycles > 0)
         {
-            Byte Ins = FetchByte( Cycles, memory );
-            switch ( Ins )
+            Byte Ins = FetchByte(Cycles, memory);
+            switch (Ins)
             {
             case INS_LDA_IM:
             {
-                Byte Value = 
-                FetchByte( Cycles, memory);
+                Byte Value =
+                    FetchByte(Cycles, memory);
                 A = Value;
                 Z = (A == 0);
-                N = (A & 0b10000000) > 0;
-            } break;
+                N = (A & 0b100000000) > 0;
+            }
+            break;
+            case INS_LDA_ZP:
+            {
+                Byte ZeroPageAddress = FetchByte(Cycles, memory);
+                ReadByte(ZeroPageAddress, memory);
+            }
             default:
             {
                 printf("Instruction Not Handled %d", Ins); //TODO: FIX THIS WEIRD ISSUE
-            }break;
+            }
+            break;
             }
         }
     }
-
 };
-
 
 int main()
 {
     Mem mem;
     CPU cpu;
-    cpu.Reset( mem );
+    cpu.Reset(mem);
     printf("CPU Reset~");
     mem[0xFFFC] = CPU::INS_LDA_IM;
     mem[0xFFFD] = 0x42;
 
-    cpu.Execute( 2, mem );
+    cpu.Execute(2, mem);
     printf("CPU reset!");
-    return 0; 
+    return 0;
 }
